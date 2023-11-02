16101aed820ab95d61ea017b0b6841a824b94f6d
Oliver Matthews <ojm@codersoffortune.net>
2023-11-02 23:49:37 +0000

    add initial test

---
+++ tests/test_designer.py
@@ -0,0 +1,8 @@
+import doenut
+
+def test_full_fact():
+    """Test full factorial output"""
+    actual = doenut.designer.full_fact({'Pressure':[50,60,70],'Temperature':[290, 320, 350],'Flow rate':[0.9,1.0]})
+    # TODO:: validate this properly
+    assert actual is not None
+    assert actual.size == 54
