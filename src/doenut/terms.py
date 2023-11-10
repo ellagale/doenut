9a7a996b5f9604d558fb7a9e2706ba3994bf0b53
Oliver Matthews <ojm@codersoffortune.net>
2023-11-10 08:51:14 +0000

    add filterable dataframe helper

---
+++ src/doenut/terms.py
@@ -0,0 +1,55 @@
+from typing import List
+
+import pandas as pd
+
+
+class Terms:
+    """
+    A piece of data, that may have a selector applied to filter it.
+    """
+
+    def __init__(self, data: pd.DataFrame) -> None:
+        if data is None:
+            raise ValueError("Data must not be null")
+        self.data = data
+        self.selector = None
+        self.indices = None
+
+    def filter(self, selector: List[str]) -> "Terms":
+        """
+        Defines the filter of what columns we want to use from the data.
+        @param selector: List of colunn names to filter with
+        @return: self for use as a builder
+        """
+        # First validate it
+        for col in selector:
+            if not col in self.data.columns:
+                raise ValueError(f"Data lacks column {col}")
+        self.selector = selector
+        self.indices = [
+            i for i, j in enumerate(self.data.columns) if j in self.selector
+        ]
+        return self
+
+    def filter_by_indices(self, indices: List[int]) -> "Terms":
+        """
+        Defines the filter of what columsn we want to use from the data
+        @param indices: list of column indices to filter with
+        @return: self for use as a builder
+        """
+        # first validate it
+        for idx in indices:
+            if idx >= len(self.data):
+                raise IndexError(f"Index {idx} is out of range for data")
+        self.indices = indices
+        self.selector = [self.data.columns[i] for i in self.indices]
+        return self
+
+    def get_filtered(self) -> pd.DataFrame:
+        """
+        Get a (shallow) copy of the data, applying the filter (if present)
+        @return: the data, filtered
+        """
+        if not self.indices:
+            return self.data
+        return self.data.iloc[:, self.indices]
---
+++ tests/test_terms.py
@@ -0,0 +1,51 @@
+import os
+import pandas as pd
+
+from doenut.terms import Terms
+
+# since pytest runs from an arbitrary location, fix that.
+os.chdir(os.path.dirname(__file__))
+df = pd.read_csv("solar_cells_1.csv")
+inputs = pd.DataFrame(
+    {
+        "Donor %": [float(x) for x in df.iloc[1:-1, 1]],
+        "Conc.": [float(x) for x in df.iloc[1:-1, 2]],
+        "Spin": [float(x) for x in df.iloc[1:-1, 3]],
+        "Add.": [float(x) for x in df.iloc[1:-1, 4]],
+    }
+)
+
+responses = pd.DataFrame({"PCE": [float(x) for x in df["PCE"][1:-1]]})
+
+
+def test_terms():
+    data = Terms(inputs)
+    assert data.data.size == 60
+    assert len(data.data.columns) == 4
+    assert data.get_filtered().equals(inputs)
+
+
+def test_filter_columns():
+    selector = ["Spin", "Add."]
+    data = Terms(inputs).filter(selector)
+    filtered_data = data.get_filtered()
+    assert filtered_data.size == 30
+    assert len(filtered_data.columns) == 2
+
+
+def test_filter_columns_by_index():
+    indices = [2, 3]
+    data = Terms(inputs).filter_by_indices(indices)
+    filtered_data = data.get_filtered()
+    assert filtered_data.size == 30
+    assert len(filtered_data.columns) == 2
+
+
+def test_filter_both_ways():
+    selector = ["Spin", "Add."]
+    data = Terms(inputs).filter(selector)
+    filtered_data = data.get_filtered()
+    indices = [2, 3]
+    indexed_data = Terms(inputs).filter_by_indices(indices)
+    filtered_indexed_data = data.get_filtered()
+    assert filtered_data.equals(filtered_indexed_data)
