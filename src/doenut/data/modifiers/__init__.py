9208e99ada76858906d7eeeca0a7bd0dad5fd10c
Oliver Matthews <ojm@codersoffortune.net>
2023-11-18 09:20:25 +0000

    fix: column_selector should allow response only filtering

--- src/doenut/data/modifiers/__init__.py
+++ src/doenut/data/modifiers/__init__.py
@@ -1,2 +1,4 @@
 from .ortho_scaler import OrthoScaler
 from .column_selector import ColumnSelector
+from .duplicate_remover import DuplicateRemover
+from .duplicate_averager import DuplicateAverager
